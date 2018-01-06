faf2e3d1249c5be97574277e1174691c9c4a0200
LINDAVERA <lindavera.lv15@gmail.com>
2018-01-06 07:07:08 -0800

    updated the contact buttons and styling

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare var window;
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
@@ -10,5 +11,9 @@ export class ContactPage {
   constructor(public navCtrl: NavController) {
 
   }
+  callIT(passedNumber){
+    //You can add some logic here
+     window.location = passedNumber;
+    }
 
 }
